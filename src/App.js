26df6917fe1c43354f29a41917a8f504ab799f6a
Yevhenii Zhdan <yevhenii7009@gmail.com>
2019-03-03 02:12:28 +0200

    remove exact

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const NoMatch = Loadable({
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route path="/" component={Home} />
       <Route component={NoMatch} />
     </Switch>
   </Router>
