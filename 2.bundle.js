66c70a1c96eeb2536c03312e03d40896e7b17d3b
Yevhenii Zhdan <yevhenii7009@gmail.com>
2019-03-03 02:03:55 +0200

    bunch of bundles

---
+++ 2.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{20:function(n,e,o){"use strict";o.r(e);var t=o(1),a=o.n(t);e.default=(()=>a.a.createElement("div",null,"404 Not Found."))}}]);
