894b58c1e51b100e073acc39573a9b4088ac9a42
Yevhenii Zhdan <yevhenii7009@gmail.com>
2019-10-26 01:12:49 +0300

    update deps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Loadable from 'react-loadable';
 import { render } from 'react-dom';
-
-const Loading = () => <div>Loading...</div>;
-
-const Home = Loadable({
-  loader: () => import('./Home'),
-  loading: Loading,
-});
-
-const Pokemon = Loadable({
-  loader: () => import('./PokemonProfile'),
-  loading: Loading,
-});
+import Home from './Home';
+import PokemonProfile from './PokemonProfile';
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/:name" component={Pokemon} />
+      <Route path="/:name" component={PokemonProfile} />
       <Route component={Home} />
     </Switch>
   </Router>
