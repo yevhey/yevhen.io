d19839450fd35f37b21975941d8dcad7372542aa
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2022-11-09 21:24:37 +0200

    it works

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { FormEvent } from 'react';
 import * as Rx from 'rxjs/operators'
-import { Observable, pipe } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Flow, UI } from '@grammarly/embrace';
 import { F } from '@grammarly/focal';
 import './Demo.css';
@@ -21,6 +20,7 @@ const Textarea = UI.Node.make<string, string>(({ state, notify }) => (
 
 const textareaFlow: Flow.For<typeof Textarea> = (actions: Observable<string>) => actions.pipe(
     Rx.startWith(''),
+    Rx.pluck('asdfsadf'),
 );
 
 const ChatActions = UI.Node.make<{}, 'attach'>(({ state, notify }) => (
@@ -29,10 +29,16 @@ const ChatActions = UI.Node.make<{}, 'attach'>(({ state, notify }) => (
     </F.button>
 ));
 
-const chatActionsFlow: Flow.For<typeof ChatActions> = (actions: Observable<'attach'>) => {
-    return actions.pipe(
-        Rx.startWith({})
-    );
-};
+const chatActionsFlow: Flow.For<typeof ChatActions> = (actions: Observable<'attach'>) => actions.pipe(
+    Rx.tap(d => console.log(d)),
+    Rx.scan((acc, a) => {
+        console.log(a);
+        if (a === 'attach') {
+            console.log('attach');
+        }
+        return acc;
+    }, 0),
+    Rx.startWith(0)
+);
 
 export const DemoForUser = () => UI.mount(Textarea, textareaFlow);
