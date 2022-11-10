1f40e2c31a37235e986c873090903282f94e0225
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2022-11-10 13:46:03 +0200

    challenge: make it compile

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,6 +1,6 @@
-import React, { FormEvent } from 'react';
+import React, {FormEvent, ReactNode} from 'react';
 import * as Rx from 'rxjs/operators'
-import { Observable } from 'rxjs';
+import {Observable, pipe} from 'rxjs';
 import { Flow, UI } from '@grammarly/embrace';
 import { F } from '@grammarly/focal';
 import './Demo.css';
@@ -9,18 +9,19 @@ import AttachIcon from './images/Attach.svg';
 // import EmojiIcon from './images/EmojiSmile.svg';
 // import SendIcon from './images/Send.svg';
 
-const Textarea = UI.Node.make<string, string>(({ state, notify }) => (
-    <F.div>
+const Textarea = UI.Node.make<string, FormEvent<HTMLTextAreaElement>>(({ state, notify }) => (
+    <F.div className="textarea-wrap">
         <F.textarea
+            placeholder="Type a message..."
             value={state}
-            onChange={e => notify(e.currentTarget.value)}
+            onChange={notify}
         />
     </F.div>
 ));
 
-const textareaFlow: Flow.For<typeof Textarea> = (actions: Observable<string>) => actions.pipe(
+const textareaFlow: Flow.For<typeof Textarea> = (actions: Observable<FormEvent<HTMLTextAreaElement>>) => actions.pipe(
     Rx.startWith(''),
-    Rx.pluck('asdfsadf'),
+    Rx.pluck('target', 'value'),
 );
 
 const ChatActions = UI.Node.make<{}, 'attach'>(({ state, notify }) => (
@@ -30,15 +31,37 @@ const ChatActions = UI.Node.make<{}, 'attach'>(({ state, notify }) => (
 ));
 
 const chatActionsFlow: Flow.For<typeof ChatActions> = (actions: Observable<'attach'>) => actions.pipe(
-    Rx.tap(d => console.log(d)),
-    Rx.scan((acc, a) => {
-        console.log(a);
-        if (a === 'attach') {
-            console.log('attach');
-        }
-        return acc;
-    }, 0),
-    Rx.startWith(0)
+    Rx.tap(d => {
+        console.log(d);
+    }),
 );
 
-export const DemoForUser = () => UI.mount(Textarea, textareaFlow);
+export const TextAreaComponent: Observable<ReactNode> = UI.mount(Textarea, textareaFlow);
+export const ActionsComponent = () => UI.mount(ChatActions, chatActionsFlow);
+
+
+const DemoChat = UI.Node.make<'textarea' | 'actions'>(({ state }) => (
+    <div className="demo">
+        <div className="demo-body" />
+        <div className="demo-footer">
+            {state}
+        </div>
+    </div>
+);
+
+export const Chat = UI.Knot.make(DemoChat, { textarea: TextAreaComponent, actions: ActionsComponent })
+
+const ChatWithoutAttachment = pipe(
+    Chat,
+    UI.patch('actions')(() => UI.node.empty),
+);
+
+const ChatWithEmojis = () => {
+
+}
+
+export const DemoForUser = pipe(
+    Chat,
+    ChatWithoutAttachment,
+    ChatWithEmojis,
+);
--- src/Demoddd.tsx
+++ src/Demoddd.tsx
@@ -1,88 +1,22 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import * as Rx from 'rxjs/operators'
-// import { Observable, pipe } from 'rxjs';
-// import { Flow, UI } from '@grammarly/embrace';
-// import { F } from '@grammarly/focal';
-// import './Demo.css';
-//
-// import AttachIcon from './images/Attach.svg';
-// import mount = UI.mount;
-// // import EmojiIcon from './images/EmojiSmile.svg';
-// // import SendIcon from './images/Send.svg';
-// const counterUI = UI.Knot.make<number, 'plus' | 'minus'>(({ state, notify }) => (
-//     <>
-//         <F.button onClick={notify('plus')}>+</F.button>
-//         {state}
-//         <F.button onClick={notify('minus')}>-</F.button>
-//     </>
-// ));
-//
-// const counterFlow: Flow.For<typeof counterUI> = (actions: Observable<'plus' | 'minus'>) => {
-//     return actions.pipe(
-//         Rx.scan((acc, a) => (a === 'plus' ? acc + 1 : acc - 1), 0),
-//         Rx.startWith(0)
-//     )
-// };
-//
-// const DemoChat = UI.Grid.make<'textarea' | 'actions'>(({ slots }) => (
-//     <div className="demo">
-//         <div className="demo-body">
-//
-//         </div>
-//         <div className="demo-footer">
-//             {slots.textarea}
-//             {slots.actions}
-//         </div>
-//     </div>
-// );
-//
-// const TextAreaComponent = UI.Knot.make<{ value: string }, 'change'>(({ state, notify }) => (
-//     <div className="textarea-wrap">
-//         <textarea
-//             placeholder="Type a message..."
-//             value={state.value}
-//             onChange={e => notify('change')(e.target.value)}
-//         />
-//     </div>
-// ));
-//
-// const ActionsComponent = UI.Knot.make<{}, 'attach'>(({ state, notify }) => (
-//     <button onClick={notify('attach')}>
-//         <AttachIcon />
-//     </button>
-// ));
-//
-// export const Chat = UI.Knot.make(DemoChat, { textarea: TextAreaComponent, actions: ActionsComponent })
-//
-// const chatFlow: Flow.For<typeof Chat> = (actions: Observable<'plus' | 'minus'>) => {
-//     return actions.pipe(
-//         Rx.scan((acc, a) => (a === 'plus' ? acc + 1 : acc - 1), 0),
-//         Rx.startWith(0)
-//     )
-// };
-//
-// const ChatApp = UI.mount(Chat, chatFlow);
-//
-// const Demoddd = () => {
-//     return (
-//         <>
-//             <span className="title">Chat</span>
-//             <DemoChat />
-//         </>
-//     );
-// };
-//
-// const DemoWithoutAttachment = UI.patch(Demoddd, {
-//     'AttachIcon': () => UI.Node.empty,
-// });
-//
-// const DemoWithEmojis = () => {
-//
-// }
-//
-// export const DemoForUser = pipe(
-//     Demoddd,
-//     DemoWithoutAttachment,
-//     DemoWithEmojis,
-// );
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as Rx from 'rxjs/operators'
+import { Observable, pipe } from 'rxjs';
+import { Flow, UI } from '@grammarly/embrace';
+import { F } from '@grammarly/focal';
+import './Demo.css';
+
+import AttachIcon from './images/Attach.svg';
+import mount = UI.mount;
+// import EmojiIcon from './images/EmojiSmile.svg';
+// import SendIcon from './images/Send.svg';
+
+const Demoddd = () => {
+    return (
+        <>
+            <span className="title">Chat</span>
+            <DemoChat />
+        </>
+    );
+};
+
