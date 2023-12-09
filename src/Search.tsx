85831411e761baba1df8e11293249a52dea44fcd
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2023-12-09 20:12:04 +0200

    one error

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,13 +1,14 @@
-import React, {forwardRef} from 'react'
+import React, { forwardRef, useRef } from 'react'
 import { useSearchInput } from './Home'
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const Search = () => {
-  const { ref, value, onChange } = useSearchInput('')
+  const ref = useRef<HTMLInputElement>(null)
+  const { value, onChange } = useSearchInput('')
 
   return (
     <>
-      Search the pokemon: {forwardRef((_, ref) => <input ref={ref} value={value} onChange={onChange} />)}
+      Search the pokemon: {forwardRef((_, ref) => <input ref={ref} value={value} onChange={onChange} />, ref)}
     </>
   )
 }
