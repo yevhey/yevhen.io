e793b4b56e033aea234bf821470c722fd8023155
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2024-06-23 21:10:25 +0300

    change credentials for azure

--- services/google-spreadsheet.ts
+++ services/google-spreadsheet.ts
@@ -1,12 +1,12 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { JWT } from 'google-auth-library';
 
-import creds from '../creds.json'
+// import creds from '../creds.json'
 
 const jwt = new JWT({
-  email: creds.client_email,
-  key: creds.private_key,
+  email: process.env.client_email,
+  key: process.env.private_key,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
-export const doc = new GoogleSpreadsheet(creds.sheet_id, jwt);
+export const doc = new GoogleSpreadsheet(process.env.sheet_id, jwt);
